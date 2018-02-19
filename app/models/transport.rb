ce4a157ee60453455213d7cdd7778efb244099bd
lockix <lockix@index.com>
2018-02-19 17:14:42 -0300

    cambios de views segundo turno

--- app/models/transport.rb
+++ app/models/transport.rb
@@ -1,5 +1,9 @@
 class Transport < ApplicationRecord
-  def to_s
-    model+" "+plate
-  end
+	validates :coupled, :presence => true
+	validates :capacity, :presence => true
+	validates :plate, :presence => true
+	validates :weight, :presence => true
+	def to_s
+		model+" "+plate
+	end
 end
