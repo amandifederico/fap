3aae1dc030902d20da36bd4c1f5d391f3d11f120
lockix <lockix@index.com>
2018-02-19 16:18:12 -0300

    views modif fede

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,6 +1,8 @@
 class Provider < ApplicationRecord
   belongs_to :city
-
+  validates :name, :presence => true
+  validates :cuit, :presence => true
+  validates :renspa, :presence => true
   def to_s
     name
   end
